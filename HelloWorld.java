ea843a36eed40dadcfa85b441cf02246f6c8a372
Stephanie-mejia <Mejia.s095373@stu.sanjac.edu>
2024-08-25 22:58:16 +0000

    Commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+public static void main (String [] args)
+{
+    System.out.println("Hello,World!");
+    System.out.println("Stephanie");
+    System.out.println("Wings");
+    System.out.println("Hellen Keller");
+    System.out.println("Cleaning the restroom");
+    System.out.println("Watching a movie");
+}
+}
