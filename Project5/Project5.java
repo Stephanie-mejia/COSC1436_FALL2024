ebb27cd738a010ea10638fad8f400ada55e687e4
Stephanie-mejia <Mejia.s095373@stu.sanjac.edu>
2024-12-14 03:35:51 +0000

    Last Commit

---
+++ Project5/Project5.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Project5
+{
+         
+        public static void main (String [] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        boolean continueCalculator = true;
+        while(continueCalculator)
+        {
+            System.out.println("Welcome to the Java Calculator");
+            System.out.println("Enter an operation(+,-,/,*, or exit");
+            String operation = kb.next();
+
+            if(operation.equals("exit"))
+            {
+                continueCalculator=false;
+                System.out.println("Exiting the Java Calculator.Bye!");
+                break;
+            }
+            
+            
+            System.out.println("Enter the first number:");
+            double number1 = kb.nextDouble();
+            System.out.println("Enter the second number:"); 
+            double number2 = kb.nextDouble();
+            double result =0;
+            boolean validOperation = true;
+            switch(operation)
+            {
+                case "+":
+                result = number1 + number2;
+                break;
+                case"-":
+                result = number1 - number2;
+                break;
+                case "*":
+                result = number1 * number2;
+                break;
+                case "/":
+                if (number2 !=0)
+                    {
+                        result =number1/number2;
+                    }
+                    else 
+                    {
+                        System.out.println("Error: Division by zero is not allowed");
+                        validOperation=false;
+                    }
+                    break;
+                default:
+                System.out.println("Invalid option.");
+                validOperation=false;
+                break;
+            }
+                if (validOperation)
+                {
+                    System.out.println("The result is " + result);
+                }
+                System.out.println("Would you like to perform another operation (yes/no)?");
+                String userChoice=kb.next();
+                if(!userChoice.equalsIgnoreCase("yes"))
+                {
+                    continueCalculator=false;
+                    System.out.println("Thank you for using the java Calculator! Goodbye!");
+                }
+
+        }
+
+        kb.close();
+    }   
+}
+
