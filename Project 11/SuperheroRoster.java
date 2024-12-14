7ae0c6e26400cf85e2ef1cbbcbc2b99338d9fd35
Stephanie-mejia <Mejia.s095373@stu.sanjac.edu>
2024-12-14 13:50:30 +0000

    Commit

---
+++ Project 11/SuperheroRoster.java
@@ -0,0 +1,13 @@
+public class SuperheroRoster
+{
+    public static void main(String [] args)
+    {
+        // declare and initialize arrays for superhero names, abilities, and power levels
+        String [] heroNames = {"Hulk", "Thor", "Spider-Man"};
+        String [] abilities = {"Strong and Big","Hammer and lightning","Webs"};
+        Int [] powerLevels = {70, 95, 80 };
+        
+        //print each hero's details using a loop
+        for (int i = 0)
+    }
+}
